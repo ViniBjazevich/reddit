b5ba1894e5ccd23293ccc640b5c580dbbc066543
Vini Bjazevich <vinibjazevich@MacBook-Pro.local>
2023-02-18 13:00:06 -0600

    feat: add user to database when using oauth

--- src/components/Modals/Authentication/OAuthButton.tsx
+++ src/components/Modals/Authentication/OAuthButton.tsx
@@ -8,11 +8,30 @@ import {
   Button,
 } from "@chakra-ui/react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { auth } from "@/firebase/initializeUI";
+import { auth, firestore } from "@/firebase/initializeUI";
 import { FIREBASE_ERRORS, ErrorKeys } from "@/firebase/errors";
+import { doc, setDoc } from "firebase/firestore";
+import { User } from "firebase/auth";
+import { useEffect } from "react";
 
 const OAuthButton = () => {
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [signInWithGoogle, userCredentials, loading, error] =
+    useSignInWithGoogle(auth);
+
+  const addUserDocument = async () => {
+    if (userCredentials) {
+      const user: User = JSON.parse(JSON.stringify(userCredentials.user));
+      const userDocRef = doc(firestore, "users", user.uid);
+
+      // set doc will create a doc if id doesn't exist or update it if it does
+      await setDoc(userDocRef, user);
+    }
+  };
+
+  useEffect(() => {
+    addUserDocument();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [userCredentials]);
 
   const handleGoogleLogin = () => {
     signInWithGoogle();
@@ -39,9 +58,7 @@ const OAuthButton = () => {
       {error && (
         <Alert status="error">
           <AlertIcon />
-          <AlertTitle>
-            {FIREBASE_ERRORS[error.message as ErrorKeys]}
-          </AlertTitle>
+          <AlertTitle>{FIREBASE_ERRORS[error.message as ErrorKeys]}</AlertTitle>
         </Alert>
       )}
       <Text color={"gray.500"} fontWeight={"700"} mt={4}>
